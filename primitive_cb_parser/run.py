b59cd012b215f405a627f87824826817d2a5a250
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2016-12-14 11:37:42 -0800

    Added prototype for primitive Couchbase parser. More like experiments on python multithreading

---
+++ primitive_cb_parser/run.py
@@ -0,0 +1,8 @@
+from server import manager
+
+manager.start(True)
+
+
+'''
+python run.py -s <cb server name> -b <bucket name>
+'''
---
+++ primitive_cb_parser/server/rest_api/__init__.py
@@ -0,0 +1,34 @@
+from multiprocessing import Queue
+from threading import Thread
+from ..communication import CommunicationObject, CommuncationProtocol
+
+
+class RestApiEngine(object):
+
+    msg_queue = Queue()
+    protocol = CommuncationProtocol()
+
+    def __init__(self, cbp_settings, dbagent):
+        self.settings = cbp_settings
+        self.dbagent = dbagent
+        self.dbagent_thread = Thread(target=dbagent.run, args=(self.msg_queue,))
+        self.dbagent_thread.daemon = True
+
+    def ping_parser_server(self):
+        return "pong\n"
+
+    def start_dbagent(self):
+        self.dbagent_thread.start()
+
+    def dbagent_get_progress(self):
+        response = ""
+        try:
+            msg_obj = self.msg_queue.get(block=True,timeout=self.protocol.DB_AGENT_RESPONSE_TIMEOUT)
+            response = "{}% completed \n".format(msg_obj.agent_progress)
+        except Exception as e:
+            return "Error connecting DB Agent \n"
+        return response
+
+
+
+
